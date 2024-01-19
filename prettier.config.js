8bbfb897d6256ef496c511fe74d002689c335f1b
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2024-01-19 15:20:19 -0500

    Configure prettier and add pkg scripts

--- prettier.config.js
+++ prettier.config.js
@@ -1,18 +1,21 @@
 module.exports = {
-	printWidth: 256,
-	singleQuote: true,
-	tabWidth: 4,
-	trailingComma: "none",
-	importOrder: [
-		"^(?:react)$",
-		"^(?:next|next/?.*)$",
-		"^(?:_|clsx|fs|lodash/?.*|path)$",
-		"<THIRD_PARTY_MODULES>",
-		"^@app/(.*)$",
-		"^[./]",
-	],
-	tailwindAttributes: ["className"],
-	plugins: ["prettier-plugin-tailwindcss"],
-	importOrderSeparation: true,
-	importOrderSortSpecifiers: true,
+    printWidth: 80,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'none',
+    importOrder: [
+        '^(?:react)$',
+        '^(?:next|next/?.*)$',
+        '^(?:_|clsx|fs|lodash/?.*|path)$',
+        '<THIRD_PARTY_MODULES>',
+        '^@app/(.*)$',
+        '^[./]'
+    ],
+    tailwindAttributes: ['className'],
+    plugins: [
+        '@trivago/prettier-plugin-sort-imports',
+        'prettier-plugin-tailwindcss'
+    ],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true
 };
