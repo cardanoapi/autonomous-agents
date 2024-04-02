0c127eb4cbad6e36ca9a409b47d56763c80c47cd
Joseph Rana <josephranamgr7@gmail.com>
2024-04-02 15:22:52 +0545

    run black lint

--- autonomous_agent/connect-agent.py
+++ autonomous_agent/connect-agent.py
@@ -4,8 +4,9 @@ import argparse
 
 default_ping_timeout = 10  # Sends a ping every 10 second
 
+
 async def connect_to_server(agent_id):
-    
+
     uri = "ws://127.0.0.1:8000/api/agent/ws"
     headers = {"agent_id": agent_id}
 
@@ -16,11 +17,15 @@ async def connect_to_server(agent_id):
             print("Received:", response)
             await asyncio.sleep(default_ping_timeout)
 
+
 async def main(agent_id):
     await connect_to_server(agent_id)
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Connect to Central WebSocket server with agent ID")
+    parser = argparse.ArgumentParser(
+        description="Connect to Central WebSocket server with agent ID"
+    )
     parser.add_argument("--agent_id", help="Agent ID to connect with", required=True)
     args = parser.parse_args()
 
