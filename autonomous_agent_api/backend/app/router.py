30a9e9a2b58286ce723014fbe8b7ea7054f2a919
Joseph Rana <josephranamgr7@gmail.com>
2024-04-02 14:58:49 +0545

    add changelog

--- autonomous_agent_api/backend/app/router.py
+++ autonomous_agent_api/backend/app/router.py
@@ -7,12 +7,18 @@ Resources:
 """
 
 from fastapi import APIRouter
-from backend.app.controllers import ready, demo, agent_router
+from backend.app.controllers import ready, demo, agent_router , agent_websocket
 
 root_api_router = APIRouter(prefix="/api")
 
+# For ready status Api
 root_api_router.include_router(ready.router, tags=["ready"])
 
+#For Demo Ping APi
 root_api_router.include_router(demo.router, tags=["test"])
 
+# For Agent CRUD operations
 root_api_router.include_router(agent_router.AgentRouter().router, tags=["agent"])
+
+# For Agent Websocket connection
+root_api_router.include_router(agent_websocket.router)
