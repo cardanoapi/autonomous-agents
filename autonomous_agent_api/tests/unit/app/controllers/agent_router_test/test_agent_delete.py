a3acaf29ad88e271314ea53ef9b24209e89be809
roshan <roshangyawali17@gmail.com>
2024-04-10 12:40:58 +0545

    test updated

--- autonomous_agent_api/tests/unit/app/controllers/agent_router_test/test_agent_delete.py
+++ autonomous_agent_api/tests/unit/app/controllers/agent_router_test/test_agent_delete.py
@@ -1,33 +1,33 @@
 import pytest
 from unittest.mock import MagicMock, AsyncMock
-from backend.app.controllers.trigger_router import TriggerRouter
+from http import HTTPStatus
+from backend.app.controllers.agent_router import AgentRouter
+from fastapi import HTTPException
+from fastapi import status
 
-from backend.app.services.trigger_service import TriggerService
 
-
-@pytest.mark.asyncio
-class TestDeleteTrigger:
+class TestAgentDelete:
     @pytest.fixture
-    def trigger_service(self):
-        return MagicMock(spec=TriggerService)
+    def agent_service(self):
+        return AsyncMock()
 
     @pytest.fixture
-    def trigger_router(self, trigger_service):
-        return TriggerRouter(trigger_service)
+    def agent_router(self, agent_service):
+        return AgentRouter(agent_service)
 
     @pytest.mark.asyncio
-    async def test_delete_trigger_with_valid_id(self, trigger_service, trigger_router):
+    async def test_delete_agent_with_valid_id(self, agent_service, agent_router):
         # Mock data
-        trigger_id = "018e8909-549b-7b9f-8fab-5499f53a8244"
+        agent_id = "018e8909-549b-7b9f-8fab-5499f53a8244"
 
-        await trigger_router.delete_trigger_by_trigger_id(trigger_id)
+        await agent_router.delete_agent(agent_id)
 
-        trigger_service.delete_by_id.assert_called_once_with(trigger_id)
+        agent_service.delete_agent.assert_called_once_with(agent_id)
 
     @pytest.mark.asyncio
-    async def test_delete_trigger_should_fail_with_no_id(self, trigger_service, trigger_router):
+    async def test_delete_agent_should_fail_with_no_id(self, agent_service, agent_router):
         # Mock data
         with pytest.raises(TypeError):
-            await trigger_router.delete_trigger_by_trigger_id()
+            await agent_router.delete_agent()
 
-            trigger_service.delete_by_id.assert_called_once_with()
+            agent_service.delete_agent.assert_called_once_with()
--- autonomous_agent_api/tests/unit/app/controllers/trigger_router_test/test_agent_delete.py
+++ autonomous_agent_api/tests/unit/app/controllers/trigger_router_test/test_agent_delete.py
@@ -1,9 +1,6 @@
-from http.client import HTTPException
-
 import pytest
 from unittest.mock import MagicMock, AsyncMock
 from backend.app.controllers.trigger_router import TriggerRouter
-from http import HTTPStatus
 
 from backend.app.services.trigger_service import TriggerService
 
@@ -19,27 +16,18 @@ class TestDeleteTrigger:
         return TriggerRouter(trigger_service)
 
     @pytest.mark.asyncio
-    async def test_delete_trigger_by_trigger_id_success(self, trigger_router, trigger_service):
-        # Define test data
-        trigger_id = "trigger_id"
-        trigger_service.delete_by_id = AsyncMock(return_value=True)
+    async def test_delete_trigger_with_valid_id(self, trigger_service, trigger_router):
+        # Mock data
+        trigger_id = "018e8909-549b-7b9f-8fab-5499f53a8244"
 
-        # Perform the test
-        response = await trigger_router.delete_trigger_by_trigger_id(trigger_id)
+        await trigger_router.delete_trigger_by_trigger_id(trigger_id)
 
-        # Assertions
-        assert response.status_code == HTTPStatus.NO_CONTENT
         trigger_service.delete_by_id.assert_called_once_with(trigger_id)
 
     @pytest.mark.asyncio
-    async def test_delete_trigger_by_trigger_id_failure(self, trigger_router, trigger_service):
-        # Define test data
-        trigger_id = 532
-        trigger_service.delete_by_id = AsyncMock(return_value=False)
+    async def test_delete_trigger_should_fail_with_no_id(self, trigger_service, trigger_router):
+        # Mock data
+        with pytest.raises(TypeError):
+            await trigger_router.delete_trigger_by_trigger_id()
 
-        # Perform the test
-        with pytest.raises(HTTPException) as exc_info:
-            await trigger_router.delete_trigger_by_trigger_id(trigger_id)
-
-        # Assertions
-        assert exc_info.value.status_code == HTTPStatus.NOT_FOUND
+            trigger_service.delete_by_id.assert_called_once_with()
