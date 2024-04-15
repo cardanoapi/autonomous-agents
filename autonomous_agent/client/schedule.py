669ca11d520938dc9257be48ca2e4a869dca9b0d
roshan <roshangyawali17@gmail.com>
2024-04-15 14:12:35 +0545

    added probablistic wrapper function to trigger the function or not before scheduling

--- autonomous_agent/client/schedule.py
+++ autonomous_agent/client/schedule.py
@@ -6,6 +6,15 @@ from actions import Add, Sub
 # Create an instance of AsyncIOScheduler
 scheduler = AsyncIOScheduler()
 
+def probabilistic_wrapper(action_func, params, probability):
+    def wrapper():
+        if random.random() <= probability:
+            result = action_func(*params)
+            print(f"Action executed with result: {result}")
+        else:
+            print(f"Action skipped due to probability check: {probability}")
+    return wrapper
+
 
 def schedule_actions(config_data):
     # Remove existing jobs from the scheduler
@@ -29,15 +38,14 @@ def schedule_actions(config_data):
 
         # Retrieve the cron expression and parameters for the action function
         cron_expression = data.get("frequency")
-        parameters = action_data.get("parameter", "")
-        # Check probability before scheduling the job
-        if random.random() <= probability:
-            # Schedule the action function using a CronTrigger
-            scheduler.add_job(
-                action_func,
-                trigger=CronTrigger.from_crontab(cron_expression),
-                args=parameters.split(","),  # Convert parameters to a list of arguments
-            )
+        parameters = action_data.get("parameter", "").split(",")  # Convert parameters to a list of arguments
+
+        # Schedule the action function using a CronTrigger and probabilistic for triggering the function
+        wrapper_function = probabilistic_wrapper(action_func, parameters, probability)
+        scheduler.add_job(
+            wrapper_function,
+            trigger=CronTrigger.from_crontab(cron_expression)
+        )
 
     # Start the scheduler if not already running
     if not scheduler.running:
