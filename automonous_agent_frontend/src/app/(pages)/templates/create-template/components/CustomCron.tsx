7cf661469b643abf7a776409911780cf09b4374c
Joseph Rana <josephranamgr7@gmail.com>
2024-05-20 16:59:53 +0545

    update custom cron component

--- automonous_agent_frontend/src/app/(pages)/templates/create-template/components/CustomCron.tsx
+++ automonous_agent_frontend/src/app/(pages)/templates/create-template/components/CustomCron.tsx
@@ -1,14 +1,31 @@
 import { NumberInput } from "@app/components/molecules/NumberInput";
+import { Input } from "@app/components/atoms/Input";
+import { useEffect, useState } from "react";
+import { onChange } from "node_modules/react-toastify/dist/core/store";
 
-export default function CustomCron(){
+export default function CustomCron(onChange:any){
+
+    
+    const [seconds , setSeconds] = useState("*")
+    const [minutes , setMinutes] = useState("*")
+    const [hours , setHours] = useState("*")
+    const [months , setMonths] = useState("*")
+    const [years , setYears] = useState("*")
+    
+    const [cron , setCron] = useState([seconds,minutes,hours,months,years])
+
+    useEffect(()=>{
+        onChange?.(cron)
+    },[setSeconds , setMinutes , setHours , setMonths , setYears])
+
     return (
         <div >
            <div className="flex items-center justify-center gap-2">
-            <NumberInput className="custom-cron-input"/>
-            <NumberInput className="custom-cron-input"/>
-            <NumberInput className="custom-cron-input"/>
-            <NumberInput className="custom-cron-input"/>
-            <NumberInput className="custom-cron-input"/>
+            <Input className="custom-cron-input custom-cron-text" value={seconds} onChange={(e) => setSeconds((e.target as HTMLInputElement).value)}/>
+            <Input className="custom-cron-input custom-cron-text" value={minutes} onChange={(e) => setMinutes((e.target as HTMLInputElement).value)}/>
+            <Input className="custom-cron-input custom-cron-text" value={hours} onChange={(e) => setHours((e.target as HTMLInputElement).value)}/>
+            <Input className="custom-cron-input custom-cron-text" value={months} onChange={(e) => setMonths((e.target as HTMLInputElement).value)}/>
+            <Input className="custom-cron-input custom-cron-text" value={years} onChange={(e) => setYears((e.target as HTMLInputElement).value)}/>
             </div>
             <div className="flex justify-center gap-3 mt-2">
                 <span className="h3">Seconds</span>
--- automonous_agent_frontend/src/app/(pages)/templates/create-template/components/TriggerTab.tsx
+++ automonous_agent_frontend/src/app/(pages)/templates/create-template/components/TriggerTab.tsx
@@ -1,4 +1,6 @@
-'use client'
+'use client';
+
+import { useEffect, useState } from 'react';
 
 import { Card } from '@app/components/atoms/Card';
 import {
@@ -7,55 +9,54 @@ import {
     TabsList,
     TabsTrigger
 } from '@app/components/molecules/Tabs';
+
 import DefaultCron from './DefaultCron';
-import { useEffect, useState } from 'react';
+import CustomCron from './CustomCron';
 
 export interface ICronSetting {
-    placeholder : string 
-    default : string[]
-    index : number
+    placeholder: string;
+    default: string[];
+    index: number;
 }
 
-const defaultCronSetting : ICronSetting[] = [
+const defaultCronSetting: ICronSetting[] = [
     {
-        placeholder : 'Minute',
-        default : ['*' , '*' , '*', '*' , '*'],
-        index : 0
+        placeholder: 'Minute',
+        default: ['*', '*', '*', '*', '*'],
+        index: 0
     },
     {
-        placeholder : 'Hour',
-        default : ['0' , '*' , '*', '*' , '*'],
-        index : 1
+        placeholder: 'Hour',
+        default: ['0', '*', '*', '*', '*'],
+        index: 1
     },
     {
-        placeholder : 'Day',
-        default : ['1' , '0' , '*', '*' , '*'],
-        index : 2
-
+        placeholder: 'Day',
+        default: ['1', '0', '*', '*', '*'],
+        index: 2
     },
     {
-        placeholder : 'Year',
-        default : ['0' ,'0' , '1', '1' ,'*'],
-        index : 4
+        placeholder: 'Year',
+        default: ['0', '0', '1', '1', '*'],
+        index: 4
     }
-]
+];
 
 export default function TriggerTab() {
+    const [cron, setCron] = useState('');
+    const [defaultSelected, setDefaultSelected] = useState('');
 
-    const [cron , setCron] = useState('')
-    const [defaultSelected , setDefaultSelected] = useState('')
-
-    useEffect(()=>{
-        console.log(cron)
-    },[cron])
+    useEffect(() => {
+        console.log(cron);
+    }, [cron]);
 
     return (
-        <Tabs defaultValue='Cron'>
-                <TabsList className='w-full justify-start rounded-none border-b-[1px] pl-0 mb-4 gap-6'>
-                    <TabsTrigger value="Cron">Cron Trigger</TabsTrigger>
-                    <TabsTrigger value="Events">Event Trigger</TabsTrigger>
-                </TabsList>
-                <Card className="bg-transparent border-brand-gray-100 w-full border-[1px] p-0 pb-5 min-h-[160px]">
+        <Tabs defaultValue="Cron">
+            <TabsList className="mb-4 w-full justify-start gap-6 rounded-none border-b-[1px] pl-0">
+                <TabsTrigger value="Cron">Cron Trigger</TabsTrigger>
+                <TabsTrigger value="Events">Event Trigger</TabsTrigger>
+            </TabsList>
+            <Card className="border-brand-gray-100 min-h-[160px] w-full border-[1px] bg-transparent p-0 pb-5">
                 <TabsContent value="Cron">
                     <Tabs defaultValue="Minute">
                         <TabsList className="w-full justify-start gap-6 rounded-none border-b-[1px] pl-4">
@@ -66,13 +67,19 @@ export default function TriggerTab() {
                             <TabsTrigger value="Custom">Custom</TabsTrigger>
                         </TabsList>
                         <div className="mt-2 flex flex-col pl-3 pt-2">
-                            { defaultCronSetting.map((item : ICronSetting) =>
-                              
-                               <TabsContent value={item.placeholder}>
-                                <DefaultCron cronSetting={item} onChange={setCron} defaultSelected={defaultSelected} setDefaultSelected={setDefaultSelected}/>
-                               </TabsContent>    
-                        )
-                            }
+                            {defaultCronSetting.map((item: ICronSetting) => (
+                                <TabsContent value={item.placeholder}>
+                                    <DefaultCron
+                                        cronSetting={item}
+                                        onChange={setCron}
+                                        defaultSelected={defaultSelected}
+                                        setDefaultSelected={setDefaultSelected}
+                                    />
+                                </TabsContent>
+                            ))}
+                            <TabsContent value="Custom">
+                                <CustomCron/>
+                            </TabsContent>
                         </div>
                     </Tabs>
                 </TabsContent>
@@ -86,7 +93,7 @@ export default function TriggerTab() {
                         </TabsContent>
                     </Tabs>
                 </TabsContent>
-        </Card>
-            </Tabs>
+            </Card>
+        </Tabs>
     );
 }
