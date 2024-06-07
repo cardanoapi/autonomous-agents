233144c01dcdafd7792f822ec90194babdab7fd5
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2024-06-07 14:43:53 +0545

    Fix pre-commit script

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -6,25 +6,29 @@ folders=("agent-manager" "agent-node" "agent-python" "api" "frontend")
 # Variable to track if any script fails
 error_occurred=false
 
+
 # Loop through each folder
 for folder in "${folders[@]}"; do
-    # Check if there are any changes in the folder
-    if git diff --quiet --exit-code "$folder"; then
-        echo "No changes detected in $folder. Skipping pre-commit script execution."
-    else
+    # Check if there are any staged changes in the folder
+    changes=$(git diff --cached --name-only --exit-code "$folder")
+    
+    # Check if changes are detected
+    if [ -n "$changes" ]; then
         echo "Changes detected in $folder. Executing pre-commit script..."
         # Execute the pre-commit script in the folder
-        (cd "$folder" && ./pre-commit)
+        (cd "$folder" && ./pre-commit.sh)
         
         # Check the exit code of the pre-commit script
         if [ $? -ne 0 ]; then
             echo "Error occurred while executing pre-commit script in $folder."
             error_occurred=true
         fi
+    else
+        echo "No changes detected in $folder. Skipping pre-commit script execution."
     fi
 done
 
-# Exit with an error code if any script failed
+# Exit with an error code if any script failed or changes are detected
 if $error_occurred; then
     exit 1
 fi
--- install-pre-commit-hook.sh
+++ install-pre-commit-hook.sh
@@ -5,19 +5,32 @@ if [ -d ".git" ]; then
   echo ".git directory exists."
 
   # Check if pre-commit.sh exists in .git/hooks
-  if [ ! -f ".git/hooks/pre-commit" ]; then
-    echo "pre-commit does not exist in .git/hooks. Copying from /hooks/pre-commit.sh..."
+  if [ -f ".git/hooks/pre-commit" ]; then
+    echo "pre-commit.sh already exists in .git/hooks. Checking if it needs to be replaced..."
     
     # Check if the source pre-commit.sh exists
     if [ -f "./hooks/pre-commit.sh" ]; then
-      cp ./hooks/pre-commit.sh .git/hooks/pre-commit
-      echo "pre-commit has been copied to .git/hooks."
+      # Compare the contents of the existing pre-commit and the source pre-commit.sh
+      if cmp -s "./hooks/pre-commit.sh" ".git/hooks/pre-commit"; then
+        echo "pre-commit.sh in .git/hooks is identical. No need to replace."
+      else
+        cp "./hooks/pre-commit.sh" ".git/hooks/pre-commit"
+        echo "pre-commit.sh has been replaced in .git/hooks."
+      fi
     else
-      echo "Source file /hooks/pre-commit.sh does not exist. Cannot copy."
+      echo "Source file ./hooks/pre-commit.sh does not exist. Cannot replace."
     fi
   else
-    echo "pre-commit.sh already exists in .git/hooks."
+    echo "pre-commit does not exist in .git/hooks. Copying from ./hooks/pre-commit.sh..."
+    
+    # Check if the source pre-commit.sh exists
+    if [ -f "./hooks/pre-commit.sh" ]; then
+      cp "./hooks/pre-commit.sh" ".git/hooks/pre-commit"
+      echo "pre-commit.sh has been copied to .git/hooks."
+    else
+      echo "Source file ./hooks/pre-commit.sh does not exist. Cannot copy."
+    fi
   fi
 else
-  echo ".git directory does not exist in the current directory.Not a git repository"
+  echo ".git directory does not exist in the current directory. Not a git repository."
 fi
