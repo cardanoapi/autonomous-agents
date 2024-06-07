724688bf217771f06f342cdbe1163c34a404f5e5
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2024-06-07 14:12:13 +0545

    Fix precommit install file

--- install-pre-commit-hook.sh
+++ install-pre-commit-hook.sh
@@ -5,13 +5,13 @@ if [ -d ".git" ]; then
   echo ".git directory exists."
 
   # Check if pre-commit.sh exists in .git/hooks
-  if [ ! -f ".git/hooks/pre-commit.sh" ]; then
-    echo "pre-commit.sh does not exist in .git/hooks. Copying from /hooks/pre-commit.sh..."
+  if [ ! -f ".git/hooks/pre-commit" ]; then
+    echo "pre-commit does not exist in .git/hooks. Copying from /hooks/pre-commit.sh..."
     
     # Check if the source pre-commit.sh exists
     if [ -f "./hooks/pre-commit.sh" ]; then
-      cp ./hooks/pre-commit.sh .git/hooks/pre-commit.sh
-      echo "pre-commit.sh has been copied to .git/hooks."
+      cp ./hooks/pre-commit.sh .git/hooks/pre-commit
+      echo "pre-commit has been copied to .git/hooks."
     else
       echo "Source file /hooks/pre-commit.sh does not exist. Cannot copy."
     fi
