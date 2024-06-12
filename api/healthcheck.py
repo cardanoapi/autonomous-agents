d2745d30e9d0159f0ce0f55782b535f0f302a469
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2024-06-12 12:36:26 +0545

    Fix health check command

---
+++ api/healthcheck.py
@@ -0,0 +1,13 @@
+import requests
+
+try:
+    response = requests.get("http://localhost:8000/api/ready")
+    if response.status_code == 200:
+        print("Healthcheck: OK")
+        exit(0)
+    else:
+        print("Healthcheck: Failed")
+        exit(1)
+except requests.exceptions.RequestException:
+    print("Healthcheck: Failed")
+    exit(1)
