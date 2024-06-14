b993a86e6c2889419ae96edf0d01ab09caff211c
Sital999 <sitalnagarkoti123@gmail.com>
2024-06-14 14:03:15 +0545

    Update path for installing shadcn components

--- frontend/src/components/atoms/Badge.tsx
+++ frontend/src/components/atoms/Badge.tsx
@@ -14,8 +14,11 @@ const badgeVariants = cva(
                 secondary:
                     'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
                 destructive:
-                    'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
-                outline: 'text-foreground'
+                    'border-transparent bg-red-500 text-white hover:bg-red-500/80',
+                outline: 'text-foreground',
+                success:
+                    '' +
+                    'border-transparent bg-green-500 text-white hover:bg-green-500/80'
             }
         },
         defaultVariants: {
