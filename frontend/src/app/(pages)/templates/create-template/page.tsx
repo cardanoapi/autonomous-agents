895a4c1dd2129066ec627526b84ee590de9dfab9
Sital999 <sitalnagarkoti123@gmail.com>
2024-06-18 16:51:34 +0545

    Update probability value before creating template

--- frontend/src/app/(pages)/templates/create-template/page.tsx
+++ frontend/src/app/(pages)/templates/create-template/page.tsx
@@ -135,7 +135,7 @@ export default function TemplateForm() {
                           defaultSelected: inputDefaultSelected,
                           configuredSettings: inputDefaultSettings,
                           parameters: inputCronParameters,
-                          probability: inputProbability
+                          probability: inputProbability / 100
                       }
                     : item
         );
