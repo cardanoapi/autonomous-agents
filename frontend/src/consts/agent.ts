677fd126db3a5aaf7977ad64afc6417413d7a6a3
Sital999 <sitalnagarkoti123@gmail.com>
2024-07-02 16:07:27 +0545

    Update AgentTrigger constant with remaining functions.

--- frontend/src/consts/agent.ts
+++ frontend/src/consts/agent.ts
@@ -22,5 +22,162 @@ export const AGENT_TRIGGER: Record<AgentTriggerFunctionType, IAgentTrigger> = {
                 data_type: 'proposal'
             }
         ]
+    },
+    'Info Action Proposal': {
+        function_name: 'Info Action Proposal',
+        parameter: [
+            {
+                name: 'anchor_url',
+                description: 'Anchor Url',
+                optional: false,
+                data_type: 'url'
+            },
+            {
+                name: 'anchor_dataHash',
+                description: 'Anchor Data Hash',
+                optional: false,
+                data_type: 'url'
+            }
+        ]
+    },
+    'Proposal New Constitution': {
+        function_name: 'Proposal New Constitution',
+        parameter: [
+            {
+                name: 'anchor_url',
+                description: 'Anchor Url',
+                optional: false,
+                data_type: 'url'
+            },
+            {
+                name: 'anchor_dataHash',
+                description: 'Anchor Data Hash',
+                optional: false,
+                data_type: 'hex'
+            },
+            {
+                name: 'newConstitution_url',
+                description: 'New Constitution Url',
+                optional: false,
+                data_type: 'url'
+            },
+            {
+                name: 'newConstitution_dataHash',
+                description: 'New Constitution Data Hash',
+                optional: false,
+                data_type: 'hex'
+            }
+        ]
+    },
+    'SendAda Token': {
+        function_name: 'SendAda Token',
+        parameter: [
+            {
+                name: 'Receiver Address',
+                description: 'Ada holder Receiver Address',
+                optional: false,
+                data_type: 'string'
+            }
+        ]
     }
 };
+
+export const AgentFunctions = [
+    {
+        function_name: 'SendAda Token',
+        parameters: [
+            {
+                name: 'Receiver Address',
+                description: 'Ada holder Receiver Address',
+                optional: false,
+                data_type: 'string'
+            }
+        ]
+    },
+    {
+        function_name: 'Proposal New Constitution',
+        parameters: [
+            {
+                name: 'anchor_url',
+                description: 'Anchor Url',
+                optional: false,
+                data_type: 'url'
+            },
+            {
+                name: 'anchor_dataHash',
+                description: 'Anchor Data Hash',
+                optional: false,
+                data_type: 'hex'
+            },
+            {
+                name: 'newConstitution_url',
+                description: 'New Constitution Url',
+                optional: false,
+                data_type: 'url'
+            },
+            {
+                name: 'newConstitution_dataHash',
+                description: 'New Constitution Data Hash',
+                optional: false,
+                data_type: 'hex'
+            }
+        ]
+    },
+    {
+        function_name: 'Info Action Proposal',
+        parameters: [
+            {
+                name: 'anchor_url',
+                description: 'Anchor Url',
+                optional: false,
+                data_type: 'url'
+            },
+            {
+                name: 'anchor_dataHash',
+                description: 'Anchor Data Hash',
+                optional: false,
+                data_type: 'url'
+            }
+        ]
+    },
+    {
+        function_name: 'Delegation',
+        parameters: [
+            {
+                name: 'drep',
+                description: 'Drep',
+                optional: true,
+                data_type: 'Drep Hash'
+            },
+            {
+                name: 'pool',
+                description: 'Stake Pool',
+                optional: true,
+                data_type: 'Drep Hash'
+            }
+        ]
+    },
+    {
+        function_name: 'Vote',
+        parameters: [
+            {
+                name: 'proposal',
+                description: 'Proposal',
+                optional: false,
+                data_type: 'proposal'
+            },
+            {
+                name: 'anchor_url',
+                description: 'Anchor Url',
+                optional: true,
+                data_type: 'url'
+            },
+            {
+                name: 'anchor_dataHash',
+                description: 'Anchor Data Hash',
+                optional: true,
+                data_type: 'hash'
+            }
+        ]
+    }
+];
