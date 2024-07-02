1b6e222a9ce0979447d7eb41750bbdf3332d7ad3
Sital999 <sitalnagarkoti123@gmail.com>
2024-07-02 19:02:56 +0545

    Add and update dto in backend for Agent

--- api/backend/app/models/agent/agent_dto.py
+++ api/backend/app/models/agent/agent_dto.py
@@ -3,8 +3,16 @@ from datetime import datetime
 from pydantic import BaseModel, Field
 from typing import List, Optional
 
+from backend.app.models import TriggerResponse
+
 
 class AgentCreateDTO(BaseModel):
     name: str = Field(..., description="Name of Agent", min_length=1)
     template_id: str
     instance: int
+
+
+class AgentUpdateDTO(BaseModel):
+    name: Optional[str] = Field(..., description="Name of Agent", min_length=1)
+    template_id: Optional[str]
+    agent_configurations: Optional[List[TriggerResponse]]
