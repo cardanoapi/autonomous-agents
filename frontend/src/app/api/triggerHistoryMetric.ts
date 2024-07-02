c1ec4931f7345efeb6647e17e3a03072a7bfefac
Joseph Rana <josephranamgr7@gmail.com>
2024-07-02 15:20:39 +0545

    Create Fetch API for triggerHistoryMetric

---
+++ frontend/src/app/api/triggerHistoryMetric.ts
@@ -0,0 +1,26 @@
+import { baseAPIurl } from './config';
+
+export interface ITriggerHistoryMetric {
+    function_name: string[];
+    successful_triggers: number;
+    unsuccessful_triggers: number;
+    skipped_triggers: number;
+    last_hour_successful_triggers: number[];
+    last_24_hour_successful_triggers: number[];
+    last_week_successful_triggers: number[];
+    today_fluctuation_rate: number;
+}
+
+export const fecthTriggerHistoryMetric = async (functions: string[]) => {
+    let fetchURL = `${baseAPIurl}/trigger-metric`;
+    if (functions.length > 0) {
+        const queryParams = functions
+            .map((func) => `function_name=${encodeURIComponent(func)}`)
+            .join('&');
+        fetchURL += `?${queryParams}`;
+    }
+    const res = fetch(fetchURL);
+    console.log(fetchURL);
+    const data = (await res).json();
+    return data;
+};
