5f8eb83e732cffa318054028adc48cb04bba1e1d
Joseph Rana <josephranamgr7@gmail.com>
2024-07-11 14:46:09 +0545

    Add ed25519 Signature Verification Function

--- api/backend/app/utils/auth.py
+++ api/backend/app/utils/auth.py
@@ -1,5 +1,6 @@
 import cbor2
 import binascii
+from nacl.signing import VerifyKey
 
 
 def get_payload_address(signature):
@@ -15,3 +16,23 @@ def get_payload_address(signature):
 
 def decode_cborhex_to_cborobj(hex_value):
     return cbor2.loads(binascii.unhexlify(hex_value))
+
+
+def verify_ed25519_signature(signature, message, public_key):
+    """
+    Signature : Hex
+    message : Hex (CoseSign1 Format)
+    PublicKey : Hex
+    """
+    try:
+        decoded_public_key = bytes.fromhex(public_key)
+        decoded_signature = bytes.fromhex(signature)
+        decoded_messsage = bytes.fromhex(message)
+
+        verify_key = VerifyKey(decoded_public_key)
+        verify_key.verify(decoded_messsage, decoded_signature)
+
+        return True
+    except Exception as e:
+        print(e)
+        return False
