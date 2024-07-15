29c6b2aaf712cd11e078245f8a2d39e26fd4462c
Joseph Rana <josephranamgr7@gmail.com>
2024-07-15 11:46:03 +0545

    Set Cookie configs & update env

--- api/backend/app/controllers/auth_router.py
+++ api/backend/app/controllers/auth_router.py
@@ -3,6 +3,8 @@ from pydantic import BaseModel
 from backend.app.utils.signed_data import verify_signed_data, extract_signed_address_from_signature_header
 from backend.app.services.user_service import UserService
 from backend.app.utils.auth import generate_jwt_token_using_user_address
+from fastapi.responses import JSONResponse
+import os
 
 
 class SignatureDataDto(BaseModel):
@@ -21,10 +23,32 @@ class AuthRouter(Routable):
         try:
             valid_data = verify_signed_data(hex_signature=signed_data.signature, hex_key=signed_data.key)
             if valid_data:
+
+                # Extract Signed Address and Creates a user using the address , if user with address already exists ,returns the user.
                 signed_address = extract_signed_address_from_signature_header(hex_signature=signed_data.signature)
                 user = await self.user_service.create_user(user_address=signed_address)
+
+                # Generate JWT Token using user Address
                 token = generate_jwt_token_using_user_address(user.address)
-                return {"token": token}
+
+                response = JSONResponse(content={"status": "verified"})
+
+                # Cookie configs
+                current_environment = os.environ.get("NODE_ENV")
+                secure = True if current_environment == "production" else False
+                domain = "api.agents.cardanoapi.io" if current_environment == "production" else "localhost"
+
+                response.set_cookie(
+                    key="access_token",
+                    value=token,
+                    samesite="None",
+                    secure=secure,
+                    expires=60 * 60 * 24,
+                    domain=domain,
+                    path="/",
+                )
+
+                return response
             else:
                 return {"Status": "Rejected", "Info": "Signed Data Signature Verification Failed"}
         except Exception as e:
