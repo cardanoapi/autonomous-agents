2805d2bb3c903a85971b11741b0d6beb9524f5ef
Joseph Rana <josephranamgr7@gmail.com>
2024-07-16 13:34:19 +0545

    update logic for new user empty agent creation

--- api/backend/app/utils/extras.py
+++ api/backend/app/utils/extras.py
@@ -1,9 +1,9 @@
 from backend.app.models.template import template_dto
-from backend.app.models.agent import agent_dto
 from backend.app.repositories.template_repository import TemplateRepository
 from backend.app.repositories.agent_repository import AgentRepository
 from backend.config.database import prisma_connection
-from backend.app.repositories import template_repository
+from backend.app.models.template.template_dto import TemplateCreateDto
+from backend.app.models.agent.agent_dto import AgentCreateDTO
 
 
 def calculate_change_rate(old_value, new_value):
@@ -18,22 +18,14 @@ async def create_empty_agent_for_user(userAddress: str):
     template_repository = TemplateRepository()
     agent_repository = AgentRepository()
 
-    empty_template_data: template_dto = {
-        "userAddress": userAddress,
-        "name": "Demo Template",
-        "description": "Auto Generated Template",
-        "template_triggers": [],
-    }
+    empty_template_data = TemplateCreateDto(
+        userAddress=userAddress, name="Empty Template", description="Auto Generated Template", template_triggers=[]
+    )
 
     async with prisma_connection.prisma.tx() as transaction:
         empty_template = await template_repository.save_template(
             transaction=transaction, template_data=empty_template_data
         )
 
-    empty_agent_data = {
-        "userAddress": userAddress,
-        "name": "Demo Agent",
-        "template_id": empty_template["id"],
-        "instance": 1,
-    }
+    empty_agent_data = AgentCreateDTO(userAddress=userAddress, name="Empty Agent", template_id=empty_template["id"])
     await agent_repository.save_agent(empty_agent_data)
