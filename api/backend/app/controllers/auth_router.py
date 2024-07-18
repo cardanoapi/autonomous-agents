5c38ffac0b62613642b8b0e2166a24e8993a6ab9
Joseph Rana <josephranamgr7@gmail.com>
2024-07-18 15:27:34 +0545

    fix : try catch issue on signed data login

--- api/backend/app/controllers/auth_router.py
+++ api/backend/app/controllers/auth_router.py
@@ -4,8 +4,9 @@ from backend.app.utils.signed_data import verify_signed_data, extract_signed_add
 from backend.app.services.user_service import UserService
 from backend.app.auth.jwt_token import generate_jwt_token_using_user_address
 from fastapi.responses import JSONResponse
-from fastapi import Response, Request
+from fastapi import Response, HTTPException
 import os
+import json
 
 
 class SignatureDataDto(BaseModel):
@@ -24,37 +25,35 @@ class AuthRouter(Routable):
 
     @post("/login")
     async def login_user(self, signed_data: SignatureDataDto):
-        try:
-            valid_data = verify_signed_data(hex_signature=signed_data.signature, hex_key=signed_data.key)
-            if valid_data:
+        valid_data = verify_signed_data(hex_signature=signed_data.signature, hex_key=signed_data.key)
+        if valid_data:
 
-                # Extract Signed Address and Creates a user using the address , if user with address already exists ,returns the user.
-                signed_address = extract_signed_address_from_signature_header(hex_signature=signed_data.signature)
-                user = await self.user_service.create_user(user_address=signed_address)
+            # Extract Signed Address and Creates a user using the address , if user with address already exists ,returns the user.
+            signed_address = extract_signed_address_from_signature_header(hex_signature=signed_data.signature)
+            user = await self.user_service.create_user(user_address=signed_address)
 
-                # Generate JWT Token using user Address
-                token = generate_jwt_token_using_user_address(user.address)
+            # Generate JWT Token using user Address
+            token = generate_jwt_token_using_user_address(user.address)
 
-                response = JSONResponse(content={"status": "verified"})
+            response = JSONResponse(
+                content={"address": user.address, "created_at": str(user.created_at), "is_superUser": user.isSuperUser}
+            )
 
-                # Cookie configs
+            # Cookie configs
 
-                response.set_cookie(
-                    key="access_token",
-                    value=token,
-                    samesite="lax",
-                    secure=self.secure,
-                    expires=60 * 60 * 24,
-                    domain=self.domain,
-                    path="/",
-                )
+            response.set_cookie(
+                key="access_token",
+                value=token,
+                samesite="lax",
+                secure=self.secure,
+                expires=60 * 60 * 24,
+                domain=self.domain,
+                path="/",
+            )
 
-                return response
-            else:
-                return {"Status": "Rejected", "Info": "Signed Data Signature Verification Failed"}
-        except Exception as e:
-            print(e)
-            return {"Status": "Rejected", "Info": "Invalid Signature Format"}
+            return response
+        else:
+            raise HTTPException(status_code=401, detail="Invalid signature")
 
     @post("/logout")
     async def logout_user(self, response: Response):
