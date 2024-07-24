3687380c92d08c779a3143854b3265d687c17033
Joseph Rana <josephranamgr7@gmail.com>
2024-07-24 12:07:05 +0545

    Feat: add typed dict user model

--- api/backend/app/models/user/user_dto.py
+++ api/backend/app/models/user/user_dto.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, TypedDict, List
+from datetime import datetime
 
 
 class SignatureDataDto(BaseModel):
@@ -10,3 +11,11 @@ class SignatureDataDto(BaseModel):
 class UserCreateDto(BaseModel):
     address: str
     isSuperUser: Optional[bool]
+
+
+class User(TypedDict):
+    address: str
+    created_at: datetime
+    updated_at: datetime
+    deleted_at: Optional[datetime]
+    isSuperUser: bool
