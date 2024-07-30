689d426984ad807351c7d7f66449e9bf27e39b6e
Joseph Rana <josephranamgr7@gmail.com>
2024-07-30 16:30:10 +0545

    Fix: update pagination to take place in query

--- api/backend/app/repositories/trigger_history_repository.py
+++ api/backend/app/repositories/trigger_history_repository.py
@@ -1,12 +1,13 @@
 from backend.config.database import prisma_connection
-from fastapi_pagination import paginate
+from fastapi_pagination import Page, paginate
+from backend.app.models.trigger_history.trigger_history_dto import TriggerHistoryDto
 
 
 class TriggerHistoryRepository:
     def __init__(self, db_connection=None):
         self.db = db_connection or prisma_connection
 
-    async def get_all_triggers_history(self, agent_id, function_name, status, success, enable_pagination=True):
+    async def get_all_triggers_history(self, agent_id, function_name, status, success, page, size):
         query = {}
         if agent_id is not None:
             query["agentId"] = agent_id
@@ -16,8 +17,20 @@ class TriggerHistoryRepository:
             query["status"] = status
         if success is not None:
             query["success"] = success
-        results = await self.db.prisma.triggerhistory.find_many(where=query, order=[{"timestamp": "desc"}])
-        return paginate(results) if enable_pagination else results
+
+        results = await self.db.prisma.triggerhistory.find_many(
+            skip=(page - 1) * size, take=size, where=query, order=[{"timestamp": "desc"}]
+        )
+        total = await self.db.prisma.triggerhistory.count(where=query)
+
+        formated_results: Page[TriggerHistoryDto] = {
+            "items": results,
+            "total": total,
+            "page": page,
+            "size": size,
+            "pages": total / size,
+        }
+        return formated_results
 
     async def get_trigger_history_by_query(self, query: dict):
         results = await self.db.prisma.triggerhistory.find_many(where=query)
