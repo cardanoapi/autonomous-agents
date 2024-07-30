baa73364553877631bdea5a7a8c5bdb6fa87a72d
Joseph Rana <josephranamgr7@gmail.com>
2024-07-30 14:18:33 +0545

    Feat: add my agents section in agents page

--- frontend/src/app/(pages)/agents/page.tsx
+++ frontend/src/app/(pages)/agents/page.tsx
@@ -18,45 +18,54 @@ import { SearchField } from '@app/components/atoms/SearchField';
 import { cn } from '@app/components/lib/utils';
 import AgentCard from '@app/components/molecules/AgentCard';
 import { SuccessToast } from '@app/components/molecules/CustomToasts';
-import { adminAccessAtom, agentCreatedAtom, agentsAtom } from '@app/store/localStore';
+import {
+    adminAccessAtom,
+    agentCreatedAtom,
+    agentsAtom,
+    currentConnectedWalletAtom
+} from '@app/store/localStore';
 
 export default function AgentsPage() {
     const [agentCreated, setAgentCreated] = useAtom(agentCreatedAtom);
     const [adminAccess] = useAtom(adminAccessAtom);
-
     const [agentsMap] = useAtom(agentsAtom);
     const [agents, setAgents] = useState<IAgent[]>([]);
+    const [filteredAgents, setFilteredAgents] = useState<IAgent[]>([]);
+    const [myAgents, setMyAgents] = useState<IAgent[]>([]);
+    const [otherAgents, setOtherAgents] = useState<IAgent[]>([]);
+    const [currentConnectedWallet] = useAtom(currentConnectedWalletAtom);
+
     useEffect(() => {
         if (agentsMap) {
             setAgents(Object.values(agentsMap));
         }
     }, [agentsMap]);
 
-    const [filteredAgents, setFilteredAgents] = useState<IAgent[]>([]);
-
     useEffect(() => {
         if (agentCreated) {
             SuccessToast('Agent Created Successfully');
             setAgentCreated(false);
         }
-        if (agents) {
-            setFilteredAgents(agents);
-        }
+        setFilteredAgents(agents);
     }, [agentCreated, agents]);
 
-    useEffect(() => {
-        if (agents) {
-            setFilteredAgents(agents);
-        }
-    }, [agents]);
-
     function handleSearch(agentName: string) {
-        const newAgents =
-            agents?.filter((agent) =>
-                agent.name.toLowerCase().includes(agentName.toLowerCase())
-            ) || [];
+        const newAgents = agents.filter((agent) =>
+            agent.name.toLowerCase().includes(agentName.toLowerCase())
+        );
         setFilteredAgents(newAgents);
     }
+
+    useEffect(() => {
+        const filteredMyAgents = filteredAgents.filter(
+            (agent) => agent.userAddress === currentConnectedWallet?.address
+        );
+        const filteredOtherAgents = filteredAgents.filter(
+            (agent) => agent.userAddress !== currentConnectedWallet?.address
+        );
+        setMyAgents(filteredMyAgents);
+        setOtherAgents(filteredOtherAgents);
+    }, [filteredAgents, currentConnectedWallet]);
 
     return (
         <>
@@ -92,41 +101,46 @@ export default function AgentsPage() {
                     </Button>
                 </Link>
             </div>
-            {/*{isLoading ? (*/}
-            {/*    <SkeletonLoadingForAgentsPage />*/}
-            {/*) : (*/}
-            <div className="mt-12 grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 3xl:grid-cols-4 4xl:grid-cols-5 5xl:grid-cols-6">
-                {Array.isArray(filteredAgents) && filteredAgents.length ? (
-                    filteredAgents.map((agent: IAgent, index) => (
-                        <AgentCard
-                            agentName={agent?.name || 'NA'}
-                            agentID={agent?.id || ''}
-                            functionCount={agent?.total_functions || 0}
-                            templateID={agent?.template_id || ''}
-                            totalTrigger={0}
-                            lastActive={agent?.last_active || 'NA'}
-                            key={index}
-                            enableEdit={adminAccess}
-                            isActive={agent?.is_active}
-                        />
-                    ))
-                ) : (
-                    <span>No Agents Found.</span>
-                )}
-            </div>
-            {/*)}*/}
+            <AgentsContainer agentsList={otherAgents} enableEdit={adminAccess} />
+            {currentConnectedWallet && myAgents.length > 0 && (
+                <>
+                    <div className="h1-new my-8">My Agents</div>
+                    <AgentsContainer agentsList={myAgents} enableEdit={true} />
+                </>
+            )}
+            {otherAgents.length == 0 && myAgents.length == 0 && (
+                <span>No Any Agents Found</span>
+            )}
         </>
     );
 }
 
-// const SkeletonLoadingForAgentsPage = () => {
-//     return (
-//         <div className={'mt-12 flex flex-wrap gap-8'}>
-//             {Array(8)
-//                 .fill(undefined)
-//                 .map((_, index) => (
-//                     <Skeleton className={'h-[240px] w-[240px]'} key={index} />
-//                 ))}
-//         </div>
-//     );
-// };
+const AgentsContainer = ({
+    agentsList,
+    enableEdit
+}: {
+    agentsList: IAgent[];
+    enableEdit: boolean;
+}) => {
+    return (
+        <div className="mt-12 grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 3xl:grid-cols-4 4xl:grid-cols-5 5xl:grid-cols-6">
+            {Array.isArray(agentsList) && agentsList.length ? (
+                agentsList.map((agent: IAgent, index) => (
+                    <AgentCard
+                        agentName={agent?.name || 'NA'}
+                        agentID={agent?.id || ''}
+                        functionCount={agent?.total_functions || 0}
+                        templateID={agent?.template_id || ''}
+                        totalTrigger={0}
+                        lastActive={agent?.last_active || 'NA'}
+                        key={index}
+                        enableEdit={enableEdit}
+                        isActive={agent?.is_active}
+                    />
+                ))
+            ) : (
+                <></>
+            )}
+        </div>
+    );
+};
