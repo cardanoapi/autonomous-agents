c8c95d7e8229099c9e9420148f6d9b82fbc3e9c7
Joseph Rana <josephranamgr7@gmail.com>
2024-09-17 15:41:46 +0545

    refactor: split templates page components

--- frontend/src/app/(pages)/templates/page.tsx
+++ frontend/src/app/(pages)/templates/page.tsx
@@ -2,19 +2,11 @@
 
 import { useEffect, useState } from 'react';
 
-import Link from 'next/link';
-
 import { useQuery } from '@tanstack/react-query';
 import { useAtom } from 'jotai';
 
 import { ITemplate, fetchTemplates } from '@app/app/api/templates';
-import DataActionBar from '@app/app/components/DataActionBar';
-import { Button } from '@app/components/atoms/Button';
-import { cn } from '@app/components/lib/utils';
 import { SuccessToast } from '@app/components/molecules/CustomToasts';
-import TemplateCard from '@app/components/molecules/TemplateCard';
-import { TemplateCardSkeleton } from '@app/components/molecules/TemplateCard';
-import { Skeleton } from '@app/components/shadcn/ui/skeleton';
 import {
     adminAccessAtom,
     currentConnectedWalletAtom,
@@ -22,22 +14,21 @@ import {
 } from '@app/store/localStore';
 import { IQueryParams } from '@app/utils/query';
 
-export default function TemplatesPage() {
+import TemplateList from './components/TemplateList';
+import TemplatesTopNav, { TemplatesTopNavSkeleton } from './components/TemplatesTopNav';
+
+const TemplatesPage = () => {
     const [templateCreated, setTemplateCreated] = useAtom(templateCreatedAtom);
-
     const [adminAccess] = useAtom(adminAccessAtom);
-
     const [currentConnectedWallet] = useAtom(currentConnectedWalletAtom);
-
     const [isFirstFetch, setIsFirstFetch] = useState<boolean>(true);
-
     const [queryParams, setQueryParams] = useState<IQueryParams>({
         page: 1,
         size: 50,
         search: ''
     });
 
-    const { data: templates = [], isLoading: isLoading } = useQuery<ITemplate[]>({
+    const { data: templates = [], isLoading } = useQuery<ITemplate[]>({
         queryKey: ['templates', queryParams],
         queryFn: () => fetchTemplates(queryParams)
     });
@@ -49,111 +40,40 @@ export default function TemplatesPage() {
         }
     }, [templateCreated, setTemplateCreated]);
 
+    // Handle search update
     const handleSearch = (value: string) => {
-        setQueryParams({ page: 1, size: 50, search: value });
+        setQueryParams((prev) => ({ ...prev, search: value, page: 1 }));
     };
 
-    const containerClass =
-        'grid grid-cols-1 gap-5 sm:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 4xl:grid-cols-5 3xl:pr-12';
-
+    // Update first fetch flag
     useEffect(() => {
-        if (isFirstFetch) {
+        if (isFirstFetch && templates.length > 0) {
             setIsFirstFetch(false);
         }
-    }, [templates]);
-
-    useEffect(() => {
-        console.log(isLoading, isFirstFetch);
-    }, [templates]);
+    }, [templates, isFirstFetch]);
 
     return (
         <>
-            {(isLoading && isFirstFetch && <TopNavSkeleton />) || (
-                <TopNav
+            {/* Top Navigation */}
+            {isLoading && isFirstFetch ? (
+                <TemplatesTopNavSkeleton />
+            ) : (
+                <TemplatesTopNav
                     onSearch={handleSearch}
                     templatesCount={templates.length}
                     adminAccess={adminAccess}
                 />
             )}
 
-            <div className="flex flex-col gap-y-[80px] pb-10 pt-5">
-                <div className="mt-2 flex flex-col gap-y-5">
-                    {isLoading && isFirstFetch ? (
-                        <div className={containerClass}>
-                            <TemplateSkeletonContainer />
-                        </div>
-                    ) : templates.length > 0 ? (
-                        <div className={containerClass}>
-                            {templates.map((template: ITemplate, index: number) => (
-                                <TemplateCard
-                                    template={template}
-                                    key={index}
-                                    enableDelete={
-                                        adminAccess === true &&
-                                        currentConnectedWallet !== null
-                                    }
-                                />
-                            ))}
-                        </div>
-                    ) : (
-                        <span>No Templates Found</span>
-                    )}
-                </div>
-            </div>
-        </>
-    );
-}
-
-const TemplateSkeletonContainer = () => {
-    return (
-        <>
-            {Array(10)
-                .fill(undefined)
-                .map((_, index: number) => {
-                    return <TemplateCardSkeleton key={index} />;
-                })}
+            {/* Template List */}
+            <TemplateList
+                templates={templates}
+                isLoading={isLoading && isFirstFetch}
+                adminAccess={adminAccess}
+                currentConnectedWallet={currentConnectedWallet}
+            />
         </>
     );
 };
 
-const TopNav = ({
-    templatesCount,
-    onSearch,
-    adminAccess
-}: {
-    onSearch: any;
-    templatesCount: number;
-    adminAccess: boolean;
-}) => {
-    return (
-        <div className="flex justify-between">
-            <div className="flex items-center justify-center gap-x-4">
-                <span className="h1-new">Templates({templatesCount})</span>
-                <DataActionBar
-                    className="h-10 min-w-[420px]"
-                    placeholder="Search Templates "
-                    onSearch={onSearch}
-                />
-            </div>
-            <Link href="/templates/create-template">
-                <Button
-                    variant="primary"
-                    className={cn('h-[36px] w-[145px]', adminAccess ? '' : '!hidden')}
-                >
-                    Create Template
-                </Button>
-            </Link>
-        </div>
-    );
-};
-
-const TopNavSkeleton = () => {
-    return (
-        <div className="flex justify-between ">
-            <div className="flex items-center gap-x-4">
-                <Skeleton className="h-8 w-[140px]" />
-                <Skeleton className="h-10 min-w-[420px]" />
-            </div>
-        </div>
-    );
-};
+export default TemplatesPage;
