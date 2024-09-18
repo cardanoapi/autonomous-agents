4eaac3a91e7c0fe32740252064333df216d082af
Joseph Rana <josephranamgr7@gmail.com>
2024-09-18 13:06:29 +0545

    feat: add search param support for internal/eternal drep

--- api/backend/app/services/drep_service.py
+++ api/backend/app/services/drep_service.py
@@ -5,6 +5,7 @@ import aiohttp
 from backend.config.api_settings import APISettings
 from backend.app.repositories.agent_repository import AgentRepository
 from backend.config.database import prisma_connection
+import base64
 
 
 class DrepService:
@@ -14,21 +15,33 @@ class DrepService:
         self.db = prisma_connection
         self.metadata_api = APISettings().METADATA_API
 
-    async def fetch_internal_dreps(self, page: int, page_size: int):
-        agents = await self.db.prisma.agent.find_many(
-            where={"deleted_at": None}, skip=(page - 1) * page_size, take=page_size, order=[{"last_active": "desc"}]
-        )
+    async def fetch_internal_dreps(self, page: int, page_size: int, search: str | None):
+
+        if search:
+            try:
+                internalDrep = await self.db.prisma.agentwalletdetails.find_first(
+                    where={"stake_key_hash": self.convert_hex_to_base64(search)}
+                )
+                agents = [
+                    await self.db.prisma.agent.find_first(where={"id": internalDrep.agent_id, "deleted_at": None})
+                ]
+            except:
+                agents = []
+        else:
+            agents = await self.db.prisma.agent.find_many(
+                where={"deleted_at": None}, skip=(page - 1) * page_size, take=page_size, order=[{"last_active": "desc"}]
+            )
         result = []
+
         async with aiohttp.ClientSession() as session:
             for agent in agents:
                 agent_details = await self.agent_repository.retreive_agent_key(agent.id)
-                drep_dict = {"agentId": agent.id, "agentName": agent.name}
+                drep_dict = {}
                 async with session.get(f"{self.gov_action_api}/drep/list?search={agent_details.drep_id}") as response:
                     response_json = await response.json()
                     if response_json["elements"]:
                         if agent_details.drep_id == response_json["elements"][0]["drepId"]:
-                            drep_dict = response_json["elements"][0] | drep_dict
-                            # result.append(response_json["elements"][0] | {"agentId": agent.id, "agentName": agent.name})
+                            drep_dict = response_json["elements"][0]
                 if drep_dict.get("metadataHash") and drep_dict.get("url"):
                     url = drep_dict.get("url")
                     metadata_hash = drep_dict.get("metadataHash")
@@ -38,25 +51,48 @@ class DrepService:
                         metadata_resp_json = await metadata_resp.json()
                         if "hash" in metadata_resp_json:
                             drep_dict["givenName"] = metadata_resp_json["metadata"]["body"]["givenName"]
-                result.append(drep_dict)
+                if drep_dict:
+                    result.append(drep_dict | {"agentId": agent.id, "agentName": agent.name})
         return result
 
-    async def fetch_external_dreps(self, page: int, page_size: int):
+    async def fetch_external_dreps(self, page: int, page_size: int, search: str | None):
+
+        if search:
+            fetchUrl = f"{self.gov_action_api}/drep/list?search={search}"
+        else:
+            fetchUrl = f"{self.gov_action_api}/drep/list?page={page}&pageSize={page_size}"
+
         async with aiohttp.ClientSession() as session:
             async with asyncio.TaskGroup() as tg:
-                async with session.get(f"{self.gov_action_api}/drep/list?page={page}&pageSize={page_size}") as response:
+                async with session.get(fetchUrl) as response:
                     response_json = await response.json()
                     for drep in response_json["elements"]:
                         if drep["metadataHash"] and drep["url"]:
                             url = drep["url"]
                             metadata_hash = drep["metadataHash"]
                             tg.create_task(self.fetch_metadata_for_drep(metadata_hash, url, drep))
+
+                            try:
+                                internalDrep = await self.db.prisma.agentwalletdetails.find_first(
+                                    where={"stake_key_hash": self.convert_hex_to_base64(drep["drepId"])}
+                                )
+                            except Exception as e:
+                                internalDrep = False
+
+                            if internalDrep:
+                                agent = await self.db.prisma.agent.find_first(
+                                    where={"id": internalDrep.agent_id, "deleted_at": None}
+                                )
+                                if agent:
+                                    drep["agentId"] = agent.id
+                                    drep["agentName"] = agent.name
+
             return {
                 "items": response_json["elements"],
                 "total": response_json["total"],
-                "page": response_json["page"],
+                "page": max(response_json["page"], 1),
                 "size": response_json["pageSize"],
-                "pages": int(response_json["total"]) // int(response_json["pageSize"]),
+                "pages": max(int(response_json["total"]) // int(response_json["pageSize"]), 1),
             }
 
     async def fetch_metadata_for_drep(self, metadata_hash: str, url: str, drep: Any):
@@ -65,3 +101,11 @@ class DrepService:
                 metadata_resp_json = await metadata_resp.json()
                 if "hash" in metadata_resp_json:
                     drep["givenName"] = metadata_resp_json.get("metadata", {}).get("body", {}).get("givenName", None)
+
+    def convert_hex_to_base64(self, hex_string):
+        try:
+            binary_data = bytes.fromhex(hex_string)
+            base64_result = base64.b64encode(binary_data).decode("utf-8")
+            return base64_result
+        except:
+            return hex_string
