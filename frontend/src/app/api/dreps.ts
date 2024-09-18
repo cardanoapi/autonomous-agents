77cba4464a2e6aa28c1bc62c8bc2b6a82b24b3cf
Joseph Rana <josephranamgr7@gmail.com>
2024-09-18 13:20:13 +0545

    feat: integrate search support for Drep Api on frontend

--- frontend/src/app/api/dreps.ts
+++ frontend/src/app/api/dreps.ts
@@ -14,12 +14,14 @@ export const fetchDreps = async (params: {
     page: number;
     pageSize: number;
     drep_type: string;
+    search: string;
 }): Promise<IDrepResponse> => {
-    const { page, pageSize, drep_type } = params;
+    const { page, pageSize, drep_type, search } = params;
     const queryString = new URLSearchParams({
         page: page.toString(),
         pageSize: pageSize.toString(),
-        drep_type: drep_type
+        drep_type: drep_type,
+        search: search
     }).toString();
 
     const res = await fetch(`${baseAPIurl}/dreps?${queryString}`);
