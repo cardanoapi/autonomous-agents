59b0a9d0430b8225fd0c90e004b151296f467d96
Sital999 <sitalnagarkoti123@gmail.com>
2024-09-24 17:08:56 +0545

    Start blockchain object in agent0manager

--- agent-manager/src/index.ts
+++ agent-manager/src/index.ts
@@ -24,6 +24,7 @@ const server = app.listen(port, async () => {
         console.log('Connection count:', manager.server.clients.size)
     }, 10000)
 
+    blockchain.start()
     blockchain.blockChain.on('extendBlock', (block) => {
         console.log(
             `[Blockchain] RollForward blockNo=${block.blockNo} hash=${block.headerHash.toString('hex')} slot=${block.slotNo}`
