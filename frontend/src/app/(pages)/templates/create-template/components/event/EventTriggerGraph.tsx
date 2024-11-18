ebbce2770f9b009995f41a8e785dfded5ab63b4a
Joseph Rana <josephranamgr7@gmail.com>
2024-11-18 15:51:49 +0545

    feat: add event trigger tab

---
+++ frontend/src/app/(pages)/templates/create-template/components/event/EventTriggerGraph.tsx
@@ -0,0 +1,183 @@
+import { useEffect, useState } from 'react';
+
+import { Canvas, Edge, EdgeData, Node, NodeData, NodeProps } from 'reaflow';
+
+import { IEventTrigger, IFilterNode } from './EventTrigger';
+
+interface NodeGraphProps {
+    data : IEventTrigger;
+    className?: string;
+    onNodeClick?: (node: Node) => any;
+    maxHeight?: number;
+    maxWidth?: number;
+    nodeStyle?: any;
+}
+
+export default function EventTriggerGraph({
+    data ,
+    className,
+    onNodeClick,
+    maxHeight,
+    maxWidth,
+    nodeStyle
+}: NodeGraphProps) {
+    const [nodes, setNodes] = useState<NodeData[]>([]);
+    const [edges, setEdges] = useState<EdgeData[]>([]);
+
+    const getNodeWidth = (value: string) => {
+        return Math.min(Math.max(value.length * 15, 60), 350);
+    };
+
+    const reduceString = (strList: string[] | string): string => {
+        if (typeof strList === 'string') return strList;
+        return strList.join('.');
+    };
+
+    useEffect(() => {
+        const newNodes: NodeData[] = [];
+        const newEdges: EdgeData[] = [];
+
+        const truncate = (text: string) => {
+            if (text.length > 50) {
+                return text.slice(0, 50) + '...';
+            }
+            return text;
+        };
+
+        const addNode = (id: string, text?: string) => {
+            newNodes.push({
+                id: id,
+                text: truncate(text ? text : id),
+                width: getNodeWidth(text ? text : id)
+            });
+        };
+
+        const addEdge = (parentId: string, childId: string) => {
+            newEdges.push({
+                id: `${parentId}-${childId}`,
+                from: `${parentId}`,
+                to: `${parentId}.${childId}`
+            });
+        };
+
+        const reduceStr = (text: string | string[]) => {
+            if (typeof text === 'string') {
+                return text;
+            }
+            return text.join('.');
+        };
+
+        const addNodesRecursively = (obj: IFilterNode, parentId: string) => {
+            if (obj.id) {
+                const currentId = parentId + '.' + reduceStr(obj.id);
+
+                let nodeText = reduceStr(obj.id) + ` ( ${obj.operator} ) `;
+                if ('value' in obj) {
+                    nodeText = nodeText + obj.value;
+                }
+
+                if (obj.negate) {
+                    nodeText = 'NOT ( ' + nodeText + ')';
+                }
+
+                addNode(currentId, nodeText);
+                addEdge(parentId, reduceStr(obj.id));
+
+                if ('children' in obj) {
+                    obj.children.forEach((childObj) => {
+                        addNodesRecursively(childObj, currentId);
+                    });
+                }
+            }
+            if (!('id' in obj) && 'children' in obj) {
+                obj.children.forEach((childObj) => {
+                    addNodesRecursively(childObj, parentId);
+                });
+            }
+        };
+
+        // Initalize Root node and edge
+        const rootId = reduceString(data.id);
+        newNodes.push({
+            id: rootId,
+            text: data.id + ` ( ${data.operator} ) `,
+            width: getNodeWidth(data.id + ` ${data.operator} `)
+        });
+
+        data.parameters.forEach((param) => {
+            // recursivelyAddNode(param, operatorId);
+            addNodesRecursively(param, rootId);
+        });
+
+        setNodes(newNodes);
+        setEdges(newEdges);
+    }, [data]);
+
+    return (
+        <Canvas
+            nodes={
+                Array.from(new Set(nodes.map((node) => JSON.stringify(node)))).map(
+                    (node) => JSON.parse(node)
+                ) as NodeData[]
+            } //ensure nodes are unique
+            edges={
+                Array.from(new Set(edges.map((edge) => JSON.stringify(edge)))).map(
+                    (edge) => JSON.parse(edge)
+                ) as EdgeData[]
+            } // ensure edges are unique
+            pannable={true}
+            panType="scroll"
+            maxHeight={maxHeight || 800}
+            maxWidth={maxWidth || 1600}
+            className={`w-full bg-[#D2E0FB] ${className || ''}`}
+            direction="RIGHT"
+            zoomable={true}
+            node={(node: NodeProps) => (
+                <Node
+                    //   onClick={(event, node) => onNodeClick && onNodeClick(node)}
+                    style={nodeStyle}
+                />
+            )}
+        />
+    );
+}
+
+// const recursivelyAddNode = (obj: any, parentId: string, text?: string) => {
+//     if (Array.isArray(obj.id)) {
+//         obj.id.forEach((idPart: string, index: number) => {
+//             const newParentId =
+//                 index > 0
+//                     ? `${parentId}.${obj.id.slice(0, index).join('.')}`
+//                     : parentId;
+
+//             const isFirst = index === 0;
+//             const isLast = index === obj.id.length - 1;
+
+//             if (isFirst) {
+//                 recursivelyAddNode({ id: idPart }, newParentId, text);
+//             } else if (isLast) {
+//                 recursivelyAddNode(
+//                     { id: idPart, value: `${obj.operator} - ${obj.value}` },
+//                     newParentId
+//                 );
+//             } else {
+//                 recursivelyAddNode({ id: idPart }, newParentId);
+//             }
+//         });
+//     } else if (typeof obj.id === 'string') {
+//         const nodeId = `${parentId}.${obj.id}`;
+//         text
+//             ? addNode(nodeId, obj.id + text)
+//             : obj.value
+//               ? addNode(nodeId, `${obj.id} : ${obj.value}`)
+//               : addNode(nodeId, obj.id);
+//         addEdge(parentId, obj.id);
+//     }
+
+//     // If obj has children, recursively add each child node
+//     else if (obj.children) {
+//         obj.children.forEach((child: any) =>
+//             recursivelyAddNode(child, parentId, ` ( ${obj.operator} ) `)
+//         );
+//     }
+// };
---
+++ frontend/src/app/(pages)/templates/create-template/components/event/demo.ts
@@ -0,0 +1,66 @@
+import { IEventTrigger } from "@api/agents";
+
+const demoData: IEventTrigger = {
+    id: 'tx',
+    negate: false,
+    operator: 'AND',
+    parameters: [
+        {
+            negate: false,
+            id: ['inputs', 'address'],
+            value: 'addr_test1qqgn33e8vc40948u4crwsq7dxk9e6kuf3amp6gf4k57n97prsqy2pgnvcwzlutpc6a2ttwnuggl36hy4vkm227zdy0gq9mj2yv',
+            operator: 'equals'
+        },
+        {
+            negate: false,
+            children: [
+                {
+                    id: ['outputs', 'value'],
+                    value: 999,
+                    negate: true,
+                    operator: 'greaterThan'
+                },
+                {
+                    id: ['outputs', 'address'],
+                    value: 'addr.....',
+                    negate: false,
+                    operator: 'equals'
+                }
+            ],
+            operator: 'AND'
+        }
+    ]
+};
+
+const nestedData: IEventTrigger = {
+    id: 'tx',
+    negate: false,
+    operator: 'AND',
+    parameters: [
+        {
+            negate: false,
+            id: ['inputs', 'address'],
+            value: 'addr_test1qqgn33e8vc40948u4crwsq7dxk9e6kuf3amp6gf4k57n97prsqy2pgnvcwzlutpc6a2ttwnuggl36hy4vkm227zdy0gq9mj2yv',
+            operator: 'equals'
+        },
+        {
+            id: 'outputs',
+            negate: false,
+            children: [
+                {
+                    id: ['value'],
+                    value: 999,
+                    negate: true,
+                    operator: 'greaterThan'
+                },
+                {
+                    id: ['address'],
+                    value: 'addr.....',
+                    negate: false,
+                    operator: 'equals'
+                }
+            ],
+            operator: 'AND'
+        }
+    ]
+};
