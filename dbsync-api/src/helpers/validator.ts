e069b1e10e83ecff803fab335bb27ecf2145bc87
Sudip Bhattarai <sudipbhattarai100@gmail.com>
2025-01-31 14:20:09 +0545

    Fix proper decoding of drep bech32

--- dbsync-api/src/helpers/validator.ts
+++ dbsync-api/src/helpers/validator.ts
@@ -2,7 +2,7 @@ import { bech32 } from "bech32";
 
 export function convertToHexIfBech32(address: string): string {
   if (!address) return ''
-  if (address.includes('stake')|| address.includes('drep')){
+  if (address.startsWith('stake')|| address.startsWith('drep')){
     const decoded = bech32.decode(address);
     const data = bech32.fromWords(decoded.words);
     return Buffer.from(data).toString('hex');
@@ -12,6 +12,68 @@ export function convertToHexIfBech32(address: string): string {
   }
   return address
 }
+
+export function decodeDrep(address:string):{credential:string,isScript?:boolean}{
+  const drep = decodeAddress(address);
+  if(drep.bech32Prefix === 'drep_script'){
+    return {
+      credential: drep.credential.toString('hex'),
+      isScript: true
+    }
+  }else{
+    if(drep.dataHeader.length >1){
+      throw new Error("Drep credential contains invalid header: "+address)
+    }
+    let isScript
+    const header=drep.dataHeader.at(0)
+    if(header == 0x22){
+      isScript = false
+    }else if (header == 0x23){
+      isScript = true
+    }else if ( header){
+      throw new Error("Drep credential contains invalid header 0x"+ header.toString(16)+ ": "+address)
+    }
+    return {
+      credential: drep.credential.toString('hex'),
+      isScript: isScript
+    }
+  }
+
+}
+
+
+export function decodeAddress(address: string): { bech32Prefix: string, dataHeader: Buffer, credential: Buffer } {
+  if (!address) return { bech32Prefix: "", dataHeader: Buffer.alloc(0), credential: Buffer.alloc(0) }; // Return empty if address is falsy
+
+  if (isHexValue(address)) {
+    // Handle the case where the address is hex-encoded (you can tweak this based on your needs)
+    const buffer = Buffer.from(address, 'hex');
+    const dataHeader = buffer.subarray(0, buffer.length - 28);
+    const credential = buffer.subarray(buffer.length - 28);
+    return { bech32Prefix: "hex", dataHeader, credential };
+  } else {
+    try {
+      // Decode the Bech32 address
+      const decoded = bech32.decode(address);
+      const data = bech32.fromWords(decoded.words);
+      const buffer = Buffer.from(data);
+
+      // Split the buffer into header and credential (last 28 bytes for credential)
+      const dataHeader = buffer.subarray(0, buffer.length - 28);
+      const credential = buffer.subarray(buffer.length - 28);
+
+      return {
+        bech32Prefix: decoded.prefix,  // Extract prefix from the Bech32 decoding result
+        dataHeader,
+        credential
+      };
+
+    } catch (e: any) {
+      throw new Error("Data is not hex or bech32: " + address);
+    }
+  }
+}
+
 
 export function isHexValue(value:string):boolean{
   const hexRegex = /^(?:[0-9a-fA-F]{56}|[0-9a-fA-F]{58})$/;
--- dbsync-api/src/repository/drep.ts
+++ dbsync-api/src/repository/drep.ts
@@ -161,143 +161,139 @@ export const fetchDrepList = async (page = 1, size = 10, search = '', status?: D
     return { items: formatResult(result), totalCount }
 }
 
-export const fetchDrepDetails = async (drepId: string) => {
+export const fetchDrepDetails = async (drepId: string,isScript?:boolean) => {
+    let scriptPart= [true,false]
+    if(isScript === true){
+        scriptPart = [true,true]
+    }else if( isScript === false) {
+        scriptPart = [false,false]
+    }
+
     const result = (await prisma.$queryRaw`
-      WITH DRepId AS (SELECT decode(${drepId}, 'hex') AS raw),
-           AllRegistrationEntries AS (SELECT drep_registration.voting_anchor_id AS voting_anchor_id,
-                                             drep_registration.deposit          AS deposit,
-                                             tx.hash                            AS tx_hash,
-                                             tx.id                              as tx_id
-                                      FROM drep_registration
-                                               CROSS JOIN DRepId
-                                               JOIN drep_hash ON drep_hash.id = drep_registration.drep_hash_id
-                                               JOIN tx ON tx.id = drep_registration.tx_id
-
-                                      WHERE drep_hash.raw = DRepId.raw
-                                      ORDER BY drep_registration.tx_id asc),
-           LatestRegistrationEntry AS (SELECT drep_registration.voting_anchor_id AS voting_anchor_id,
-                                              drep_registration.deposit          AS deposit,
-                                              tx.hash                            AS tx_hash
-                                       FROM drep_registration
-                                                CROSS JOIN DrepId
-                                                JOIN drep_hash ON drep_hash.id = drep_registration.drep_hash_id
-                                                JOIN tx ON tx.id = drep_registration.tx_id
-                                       WHERE drep_hash.raw = DRepId.raw
-                                       ORDER BY drep_registration.tx_id DESC
-                                       LIMIT 1),
-           IsRegisteredAsDRep AS (SELECT (LatestRegistrationEntry.deposit IS NULL
-               OR LatestRegistrationEntry.deposit > 0)
-                                             AND LatestRegistrationEntry.voting_anchor_id IS NOT NULL AS value
-                                  FROM LatestRegistrationEntry),
-           IsRegisteredAsSoleVoter AS (SELECT (LatestRegistrationEntry.deposit IS NULL
-               OR LatestRegistrationEntry.deposit > 0)
-                                                  AND LatestRegistrationEntry.voting_anchor_id IS NULL AS value
-                                       FROM LatestRegistrationEntry),
-           CurrentDeposit AS (SELECT GREATEST(drep_registration.deposit, 0) AS value
-                              FROM drep_registration
-                                       JOIN drep_hash
-                                            ON drep_hash.id = drep_registration.drep_hash_id
-                                       CROSS JOIN DRepId
-                              WHERE drep_registration.deposit IS NOT NULL
-                                AND drep_hash.raw = DRepId.raw
-                              ORDER BY drep_registration.tx_id DESC
-                              LIMIT 1),
-           WasRegisteredAsDRep AS (SELECT (EXISTS (SELECT *
-                                                   FROM drep_registration
-                                                            JOIN drep_hash ON drep_hash.id = drep_registration.drep_hash_id
-                                                            CROSS JOIN DRepId
-                                                   WHERE drep_hash.raw = DRepId.raw
-                                                     AND drep_registration.voting_anchor_id IS NOT NULL)) AS value),
-           WasRegisteredAsSoleVoter AS (SELECT (EXISTS (SELECT *
-                                                        FROM drep_registration
-                                                                 JOIN drep_hash ON drep_hash.id = drep_registration.drep_hash_id
-                                                                 CROSS JOIN DRepId
-                                                        WHERE drep_hash.raw = DRepId.raw
-                                                          AND drep_registration.voting_anchor_id IS NULL)) AS value),
-           CurrentMetadata AS (SELECT voting_anchor.url                      AS url,
-                                      encode(voting_anchor.data_hash, 'hex') AS data_hash
-                               FROM LatestRegistrationEntry
-                                        LEFT JOIN voting_anchor
-                                                  ON voting_anchor.id = LatestRegistrationEntry.voting_anchor_id
-                               LIMIT 1),
-           CurrentVotingPower AS (SELECT amount AS amount
-                                  FROM drep_hash
-                                           JOIN DRepId
-                                                ON drep_hash.raw = DRepId.raw
-                                           LEFT JOIN drep_distr ON drep_distr.hash_id = drep_hash.id
-                                  ORDER BY drep_distr.epoch_no DESC
-                                  LIMIT 1),
-           DRepRegister AS (SELECT encode(AllRegistrationEntries.tx_hash, 'hex') as tx_hash,
-                                   AllRegistrationEntries.tx_id
-                            FROM (SELECT 1) AS dummy
-                                     LEFT JOIN
-                                 AllRegistrationEntries
-                                 ON AllRegistrationEntries.voting_anchor_id IS NOT NULL and
-                                    not (coalesce(deposit, 0) < 0)
-                            ORDER BY AllRegistrationEntries.tx_id DESC
-                            LIMIT 1),
-           DRepRetire AS (SELECT encode(AllRegistrationEntries.tx_hash, 'hex') as tx_hash,
-                                 AllRegistrationEntries.tx_id                  as tx_id
-                          FROM DRepRegister
-                                   LEFT JOIN
-                               AllRegistrationEntries
-                               ON (AllRegistrationEntries.deposit < 0
-                                   OR AllRegistrationEntries.voting_anchor_id IS NULL)
-                                   and AllRegistrationEntries.tx_id > DRepRegister.tx_id
-                          ORDER BY AllRegistrationEntries.tx_id asc
-                          LIMIT 1),
-           SoleVoterRegister AS (SELECT encode(AllRegistrationEntries.tx_hash, 'hex') as tx_hash,
-                                        AllRegistrationEntries.tx_id
-                                 FROM (SELECT 1) AS dummy
-                                          LEFT JOIN
-                                      AllRegistrationEntries
-                                      ON AllRegistrationEntries.voting_anchor_id IS NULL and
-                                         not (coalesce(deposit, 0) < 0)
-                                 ORDER BY AllRegistrationEntries.tx_id DESC
+WITH dreps AS ( SELECT  id,has_script FROM drep_hash
+               WHERE drep_hash.raw = decode(${drepId}, 'hex') and  (has_script = ${scriptPart[0]} or has_script = ${scriptPart[1]})),
+               drep as  (SELECT  dreps.* from drep_registration  join dreps  on dreps.id = drep_registration.drep_hash_id   where deposit > 0 order by tx_id desc limit 1),
+               AllRegistrationEntries AS (SELECT drep_registration.voting_anchor_id AS voting_anchor_id,
+                                       drep_registration.deposit          AS deposit,
+                                       tx.hash                            AS tx_hash,
+                                       tx.id                              as tx_id
+                                FROM drep_registration
+                                         JOIN drep ON drep.id = drep_registration.drep_hash_id
+                                         JOIN tx ON tx.id = drep_registration.tx_id
+                                ORDER BY drep_registration.tx_id asc),
+     LatestRegistrationEntry AS (SELECT drep_registration.voting_anchor_id AS voting_anchor_id,
+                                        drep_registration.deposit          AS deposit,
+                                        tx.hash                            AS tx_hash
+                                 FROM drep_registration
+                                          JOIN drep ON drep.id = drep_registration.drep_hash_id
+                                          JOIN tx ON tx.id = drep_registration.tx_id
+                                 ORDER BY drep_registration.tx_id DESC
                                  LIMIT 1),
-           SoleVoterRetire AS (SELECT encode(AllRegistrationEntries.tx_hash, 'hex') as tx_hash
-                               FROM SoleVoterRegister
-                                        LEFT JOIN
-                                    AllRegistrationEntries
-                                    ON (AllRegistrationEntries.deposit < 0
-                                        OR AllRegistrationEntries.voting_anchor_id IS NOT NULL)
-                                        AND AllRegistrationEntries.tx_id > SoleVoterRegister.tx_id
-                               ORDER BY AllRegistrationEntries.tx_id asc
-                               LIMIT 1),
-           IsScriptHash AS (SELECT EXISTS (SELECT drep_hash.has_script
-                                           FROM drep_hash
-                                                    CROSS JOIN DRepId
-                                           WHERE drep_hash.raw = DRepId.raw
-                                             AND drep_hash.has_script = true) AS has_script),
-           DrepDetails AS (SELECT IsScriptHash.has_script        as "hasScript",
-                                  IsRegisteredAsDRep.value       as "isRegisteredAsDRep",
-                                  WasRegisteredAsDRep.value      as "wasRegisteredAsDRep",
-                                  IsRegisteredAsSoleVoter.value  as "isRegisteredAsSoleVoter",
-                                  WasRegisteredAsSoleVoter.value as "wasRegisteredAsSoleVoter",
-                                  CurrentDeposit.value           as "deposit",
-                                  CurrentMetadata.url            as "url",
-                                  CurrentMetadata.data_hash      as "dataHash",
-                                  CurrentVotingPower.amount      as "votingPower",
-                                  DRepRegister.tx_hash           as "dRepRegisterTxHash",
-                                  DRepRetire.tx_hash             as "dRepRetireTxHash",
-                                  SoleVoterRegister.tx_hash      as "soleVoterRegisterTxHash",
-                                  SoleVoterRetire.tx_hash        as "soleVoterRetireTxHash"
-                           FROM IsRegisteredAsDRep
-                                    CROSS JOIN IsRegisteredAsSoleVoter
-                                    CROSS JOIN WasRegisteredAsDRep
-                                    CROSS JOIN WasRegisteredAsSoleVoter
-                                    CROSS JOIN CurrentDeposit
-                                    CROSS JOIN CurrentMetadata
-                                    CROSS JOIN CurrentVotingPower
-                                    CROSS JOIN DRepRegister
-                                    CROSS JOIN DRepRetire
-                                    CROSS JOIN SoleVoterRegister
-                                    CROSS JOIN SoleVoterRetire
-                                    CROSS JOIN IsScriptHash)
+     IsRegisteredAsDRep AS (SELECT (LatestRegistrationEntry.deposit IS NULL
+         OR LatestRegistrationEntry.deposit > 0)
+                                       AND LatestRegistrationEntry.voting_anchor_id IS NOT NULL AS value
+                            FROM LatestRegistrationEntry),
+     IsRegisteredAsSoleVoter AS (SELECT (LatestRegistrationEntry.deposit IS NULL
+         OR LatestRegistrationEntry.deposit > 0)
+                                            AND LatestRegistrationEntry.voting_anchor_id IS NULL AS value
+                                 FROM LatestRegistrationEntry),
+     CurrentDeposit AS (SELECT GREATEST(drep_registration.deposit, 0) AS value
+                        FROM drep_registration
+                                 JOIN drep
+                                      ON drep.id = drep_registration.drep_hash_id
+                        WHERE drep_registration.deposit IS NOT NULL
+                        ORDER BY drep_registration.tx_id DESC
+                        LIMIT 1),
+     WasRegisteredAsDRep AS (SELECT (EXISTS (SELECT *
+                                             FROM drep_registration
+                                                      JOIN drep ON drep.id = drep_registration.drep_hash_id
+                                             WHERE
+                                                drep_registration.voting_anchor_id IS NOT NULL)) AS value),
+     WasRegisteredAsSoleVoter AS (SELECT (EXISTS (SELECT *
+                                                  FROM drep_registration
+                                                           JOIN drep ON drep.id = drep_registration.drep_hash_id
+                                                  WHERE
+                                                     drep_registration.voting_anchor_id IS NULL)) AS value),
+     CurrentMetadata AS (SELECT voting_anchor.url                      AS url,
+                                encode(voting_anchor.data_hash, 'hex') AS data_hash
+                         FROM LatestRegistrationEntry
+                                  LEFT JOIN voting_anchor
+                                            ON voting_anchor.id = LatestRegistrationEntry.voting_anchor_id
+                         LIMIT 1),
+     CurrentVotingPower AS (SELECT amount AS amount
+                            FROM drep
+                                     LEFT JOIN drep_distr ON drep_distr.hash_id = drep.id
+                            ORDER BY drep_distr.epoch_no DESC
+                            LIMIT 1),
+     DRepRegister AS (SELECT encode(AllRegistrationEntries.tx_hash, 'hex') as tx_hash,
+                             AllRegistrationEntries.tx_id
+                      FROM (SELECT 1) AS dummy
+                               LEFT JOIN
+                           AllRegistrationEntries
+                           ON AllRegistrationEntries.voting_anchor_id IS NOT NULL and
+                              not (coalesce(deposit, 0) < 0)
+                      ORDER BY AllRegistrationEntries.tx_id DESC
+                      LIMIT 1),
+     DRepRetire AS (SELECT encode(AllRegistrationEntries.tx_hash, 'hex') as tx_hash,
+                           AllRegistrationEntries.tx_id                  as tx_id
+                    FROM DRepRegister
+                             LEFT JOIN
+                         AllRegistrationEntries
+                         ON (AllRegistrationEntries.deposit < 0
+                             OR AllRegistrationEntries.voting_anchor_id IS NULL)
+                             and AllRegistrationEntries.tx_id > DRepRegister.tx_id
+                    ORDER BY AllRegistrationEntries.tx_id asc
+                    LIMIT 1),
+     SoleVoterRegister AS (SELECT encode(AllRegistrationEntries.tx_hash, 'hex') as tx_hash,
+                                  AllRegistrationEntries.tx_id
+                           FROM (SELECT 1) AS dummy
+                                    LEFT JOIN
+                                AllRegistrationEntries
+                                ON AllRegistrationEntries.voting_anchor_id IS NULL and
+                                   not (coalesce(deposit, 0) < 0)
+                           ORDER BY AllRegistrationEntries.tx_id DESC
+                           LIMIT 1),
+     SoleVoterRetire AS (SELECT encode(AllRegistrationEntries.tx_hash, 'hex') as tx_hash
+                         FROM SoleVoterRegister
+                                  LEFT JOIN
+                              AllRegistrationEntries
+                              ON (AllRegistrationEntries.deposit < 0
+                                  OR AllRegistrationEntries.voting_anchor_id IS NOT NULL)
+                                  AND AllRegistrationEntries.tx_id > SoleVoterRegister.tx_id
+                         ORDER BY AllRegistrationEntries.tx_id asc
+                         LIMIT 1),
+     IsScriptHash AS (SELECT EXISTS (SELECT drep.has_script
+                                     FROM drep
+                                     WHERE drep.has_script = true) AS has_script),
+     DrepDetails AS (SELECT IsScriptHash.has_script        as "hasScript",
+                            IsRegisteredAsDRep.value       as "isRegisteredAsDRep",
+                            WasRegisteredAsDRep.value      as "wasRegisteredAsDRep",
+                            IsRegisteredAsSoleVoter.value  as "isRegisteredAsSoleVoter",
+                            WasRegisteredAsSoleVoter.value as "wasRegisteredAsSoleVoter",
+                            CurrentDeposit.value           as "deposit",
+                            CurrentMetadata.url            as "url",
+                            CurrentMetadata.data_hash      as "dataHash",
+                            CurrentVotingPower.amount      as "votingPower",
+                            DRepRegister.tx_hash           as "dRepRegisterTxHash",
+                            DRepRetire.tx_hash             as "dRepRetireTxHash",
+                            SoleVoterRegister.tx_hash      as "soleVoterRegisterTxHash",
+                            SoleVoterRetire.tx_hash        as "soleVoterRetireTxHash"
+                     FROM IsRegisteredAsDRep
+                              CROSS JOIN IsRegisteredAsSoleVoter
+                              CROSS JOIN WasRegisteredAsDRep
+                              CROSS JOIN WasRegisteredAsSoleVoter
+                              CROSS JOIN CurrentDeposit
+                              CROSS JOIN CurrentMetadata
+                              CROSS JOIN CurrentVotingPower
+                              CROSS JOIN DRepRegister
+                              CROSS JOIN DRepRetire
+                              CROSS JOIN SoleVoterRegister
+                              CROSS JOIN SoleVoterRetire
+                              CROSS JOIN IsScriptHash)
 
-      SELECT json_build_object(
-                     'drep_details', (SELECT row_to_json(DrepDetails) FROM DrepDetails)
-             ) as result;
+SELECT json_build_object(
+               'drep_details', (SELECT row_to_json(DrepDetails) FROM DrepDetails)
+       ) as result;
   `) as Record<string, any>[]
     return result[0].result.drep_details ? result[0].result.drep_details : result[0].result
 }
