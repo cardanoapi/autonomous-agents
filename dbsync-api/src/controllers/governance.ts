cea9bacdb5b4863910cee90b54487542f7142920
Reeshav Acharya <reeshav99acharya@gmail.com>
2025-02-04 14:32:50 +0545

    Complete cache implementation of active and live total stake

--- dbsync-api/src/controllers/governance.ts
+++ dbsync-api/src/controllers/governance.ts
@@ -1,14 +1,41 @@
 import { PrismaClient } from '@prisma/client'
 import { Request, Response, Router } from 'express'
 import { handlerWrapper } from '../errors/AppError'
-import { fetchActiveToalStake } from '../repository/governance'
+import { fetchActiveToalStake, fetchLiveTotalStake } from '../repository/governance'
+import NodeCache from 'node-cache'
+
 
 const router = Router()
+const cache = new NodeCache({ stdTTL: 3600, checkperiod: 600 })
 
-const getToalActiveStake = async (req: Request, res: Response): Promise<any> => {
+const updateCache = async () => {
+    const activeTotalStake = await fetchActiveToalStake()
+    const liveTotalStake = await fetchLiveTotalStake()
+
+    cache.set('activeTotalStake', activeTotalStake)
+    cache.set('liveTotalStake', liveTotalStake)
+}
+
+updateCache()
+setInterval(updateCache, 60 * 60 * 1000) 
+
+const getToalActiveStake = async (req: Request, res: Response) => {
+    const cachedData = cache.get('activeTotalStake')
+    if (cachedData) return res.status(200).json(cachedData)
     const result = await fetchActiveToalStake()
+    cache.set('activeTotalStake', result)
+    return res.status(200).json(result)
+}
+
+const getToalLiveStake = async (req: Request, res: Response) => {
+    const cachedData = cache.get('liveTotalStake')
+    if (cachedData) return res.status(200).json(cachedData)
+    const result = await fetchLiveTotalStake()
+    cache.set('liveTotalStake', result)
     return res.status(200).json(result)
 }
 
 router.get('/active/total-stake', handlerWrapper(getToalActiveStake))
+router.get('/live/total-stake', handlerWrapper(getToalLiveStake))
+
 export default router
