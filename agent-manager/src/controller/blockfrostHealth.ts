d963ee3cba5b11817b47ddd3ed787ce459221e1b
Sital999 <sitalnagarkoti123@gmail.com>
2025-02-04 12:12:02 +0545

    fix: Update blockfrost healthCheckError msg log

--- agent-manager/src/controller/blockfrostHealth.ts
+++ agent-manager/src/controller/blockfrostHealth.ts
@@ -4,23 +4,54 @@ import environments from '../config/environments'
 
 const router = Router()
 
+async function callBlockFrostHealthCheckEndpoint(url: string) {
+    return fetch(url)
+        .catch((e) => {
+            const a = {
+                statusCode: 500,
+                message: e.message ? e.message : e,
+            }
+            return Promise.reject(a)
+        })
+        .then((res) => {
+            if (res.status === 200) {
+                return res
+            } else {
+                return res.text().then((txt) => {
+                    let json: any
+                    try {
+                        json = JSON.parse(txt)
+                    } catch (e) {
+                        return Promise.reject({
+                            statusCode: res.status,
+                            message: txt,
+                        })
+                    }
+                    if (json) {
+                        return Promise.reject({
+                            statusCode: res.status,
+                            message: json.message ? json.message : txt,
+                        })
+                    } else {
+                        return Promise.reject({
+                            statusCode: res.status,
+                            message: txt,
+                        })
+                    }
+                })
+            }
+        })
+}
+
 async function blockfrostHealthCheck(req: Request, res: Response) {
     const url = `https://cardano-${environments.networkName}.blockfrost.io/api/v0/health`
     try {
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                project_id: environments.blockFrostApiKey,
-            },
-        })
-        const respJson = await response.json()
-        if (response.status != 200) {
-            console.log('BlockFrostCheckError : ', respJson)
-        }
-        return res.status(response.status).send(respJson)
-    } catch (err: any) {
-        console.log('BlockFrostCheckError : ', err)
-        return res.status(500).send({ error: err.message ? err.message : err })
+        const blockFrostHealthCheckResponse = await callBlockFrostHealthCheckEndpoint(url)
+        const blockFrostHealthCheckResponseJson = await blockFrostHealthCheckResponse.json()
+        return res.status(200).send(blockFrostHealthCheckResponseJson)
+    } catch (e: any) {
+        console.error('BlockFrostHealthCheckError : ', e.message ? e.message : e)
+        return res.status(e.statusCode).send({ error: e.message ? e.message : e })
     }
 }
 
